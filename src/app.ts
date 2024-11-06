e4b851e1d31f7cff8c11e8fdaae3b84978b579bc
Matthew Jordan <matthew.jordan@kainos.com>
2024-11-06 13:17:49 +0000

    YJ_006: Created Template Site

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,16 @@ import session from "express-session";
 
 import { getAllDatabases } from "./controllers/TestController";
 
+const path = require('path');
+
 const app = express();
 
-nunjucks.configure('views', {
-    autoescape: true,
-    express: app
+nunjucks.configure([
+  "node_modules/govuk-frontend/dist",
+  "views"
+], {
+  autoescape: true,
+  express: app
 });
 
 app.use(bodyParser.json())
@@ -17,7 +22,17 @@ app.use(bodyParser.urlencoded({
   extended: true
 }))
 
-app.use(session({ secret: 'SUPER_SECRET', cookie: { maxAge: 28800000 }}));
+app.use(session({ secret: 'SUPER_SECRET', cookie: { maxAge: 28800000 } }));
+
+console.log(path.join(__dirname, '/node_modules/govuk-frontend/dist/govuk/assets'));
+
+app.use('/stylesheets/govuk-frontend.min.css',
+  express.static(path.join(__dirname, '../node_modules/govuk-frontend/dist/govuk/govuk-frontend.min.css'))
+);
+app.use('/javascripts/govuk-frontend.min.js',
+  express.static(path.join(__dirname, '../node_modules/govuk-frontend/dist/govuk/govuk-frontend.min.js'))
+);
+app.use('/assets', express.static(path.join(__dirname, '../node_modules/govuk-frontend/dist/govuk/assets')));
 
 declare module "express-session" {
   interface SessionData {
@@ -26,7 +41,7 @@ declare module "express-session" {
 }
 
 app.listen(3000, () => {
-    console.log('Server started on port 3000');
+  console.log('Server started on port 3000');
 });
 
 app.get('/', getAllDatabases);
